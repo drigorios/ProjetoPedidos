45567a3c77757998ebd928a118ae0b4d63dc1c49
rodrigo <alunodesign05@gmail.com>
2024-01-02 20:55:09 -0300

    PDO

--- index.php
+++ index.php
@@ -1,33 +1,25 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Pedidos</title>
-    <link rel="stylesheet" href="index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Meus pedidos:</h1>
+    <h1>Meus Pedidos</h1>
     <form action="salvar.php" method="post">
-        <label>Data</label> <br>
-        <input type="date" name="data">
-        <label>Cliente</label><br><br>
-        <input type="text" name="cliente">
-        <label>Produto</label><br><br>
-        <input type="text" name="produto">
-        <label>Valor</label><br><br>
-        <input type="number" name="valor">
+        <label for="data" class="form-label">Data: </label><input type="datetime-local" name="data" id="data">
+        <label for="cliente">Cliente: </label><input type="text" name="cliente" id="cliente">
+        <label for="produto">Produto: </label><input type="text" name="produto" id="produto">
+        <label for="valor">Valor: </label><input type="number" name="valor" id="valor">
         <input type="submit" value="Salvar">
     </form>
     <hr>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
-    <h2>Meus pedidos:</h2>
+
     <div class="table-responsive">
         <table class="table table-primary">
             <thead>
@@ -37,28 +29,62 @@
                     <th scope="col">Produto</th>
                     <th scope="col">Valor</th>
                     <th scope="col">Ações</th>
-
                 </tr>
             </thead>
             <tbody>
+                <?php
+                //Conecta ao banco de dados usando PDO
+                $host = 'localhost';
+                $dbname = 'pedidos';
+                $username = 'root';
+                $password = '';
+                try {
+                    $pdo = new PDO("mysql:host=$host;dbname=$dbname;username=$username;password=$password;");
+                } catch (PDOException $e) {
+                    die("Erro ao conectar ao banco de dados." . $e->getMessage());
+                }
+
+                // Selecionar n sei sei nao
+                $sql = "SELECT * FROM pedidos";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute();
+
+                //Executar//
+                while ($row = $stmt->fetch()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["data"] . "</td>";
+                    echo "<td>" . $row["cliente"] . "</td>";
+                    echo "<td>" . $row["produto"] . "</td>";
+                    echo "<td>" . $row["valor"] . "</td>";
+                    echo "<td>";
+                    echo "<a name='editar' id='editar' class='btn btn-warning' href='editar.php' role='button'>".$row [id] ."Editar" ."</a>";
+                    echo "<a name='excluir' id='excluir' class='btn btn-danger' href='excluir.php' role='button'>".$row [id] ."Excluir</a>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                ?>
                 <tr class="">
-                    <td scope="row">23/11/23</td>
+                    <td scope="row">23/11/2023</td>
                     <td>José</td>
                     <td>Mesa</td>
-                    <td>23,00</td>
-                    <td><a name="editar" id="editar" class="btn btn-warning" href="#" role="button">Teste</a>
+                    <td>R$ 23,00</td>
+                    <td>
+                        <a name="editar" id="editar" class="btn btn-warning" href="#" role="button">Editar</a>
+                        <a name="excluir" id="excluir" class="btn btn-danger" href="#" role="button">Excluir</a>
                     </td>
                 </tr>
-                <tr>
+                <tr class="">
                     <td scope="row">Item</td>
                     <td>Item</td>
                     <td>Item</td>
-                    <td>Item</td>
-                    <td><a name="excluir" id="excluir" class="btn btn-danger" href="#" role="button">Teste</a></td>
                 </tr>
             </tbody>
         </table>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
