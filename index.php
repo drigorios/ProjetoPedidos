49401e271e94ad8b0741cbbce9b24f19f4347674
rodrigo <alunodesign05@gmail.com>
2024-01-03 19:04:13 -0300

    git push

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Pedidos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-
 <body>
     <h1>Meus Pedidos</h1>
     <form action="salvar.php" method="post">
@@ -20,71 +17,71 @@
     </form>
     <hr>
 
-    <div class="table-responsive">
-        <table class="table table-primary">
-            <thead>
-                <tr>
-                    <th scope="col">Data</th>
-                    <th scope="col">Cliente</th>
-                    <th scope="col">Produto</th>
-                    <th scope="col">Valor</th>
-                    <th scope="col">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                //Conecta ao banco de dados usando PDO
-                $host = 'localhost';
-                $dbname = 'pedidos';
-                $username = 'root';
-                $password = '';
-                try {
-                    $pdo = new PDO("mysql:host=$host;dbname=$dbname;username=$username;password=$password;");
-                } catch (PDOException $e) {
-                    die("Erro ao conectar ao banco de dados." . $e->getMessage());
-                }
+<div
+    class="table-responsive"
+>
+    <table
+        class="table table-primary">
+        <thead>
+            <tr>
+                <th scope="col">Data</th>
+                <th scope="col">Cliente</th>
+                <th scope="col">Produto</th>
+                <th scope="col">Valor</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            // Conecta ao banco de dados usando PDO
+            $host = 'localhost';
+            $dbname = 'pedidos';
+            $username = 'root';
+            $password = '';
+            try {
+                $pdo = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+            } catch (PDOException $e) {
+                die ("Erro ao conectar ao banco de dados".$e->getMessage());
+            }
+            // Selecionar os dados da tabela pedidos
+            $sql ="SELECT * FROM pedidos";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(); 
+            //Exibir os produtos em uma tabela
+            while ($row=$stmt->fetch()) {
+                echo "<tr>";
+                echo "<td>".$row['data']."</td>";
+                echo "<td>".$row['cliente']."</td>";
+                echo "<td>".$row['produto']."</td>";
+                echo "<td>".$row['valor']."</td>";
+                echo "<td>";
+                echo "<a href='editar.php?id=".$row['id'] ."'>Editar</a>";
+                echo "<a href='excluir.php?id=".$row['id'] ."'>Excluir</a>";
+                echo "</td>";
+                echo "</tr>";               
+            }
+            ?>
+           
+<!-- 
+           <td>
+               <a name='editar' id='editar' class='btn btn-warning' href='editar.php?id=' role='button1'>Editar</a>;
+               <a name="excluir" id="excluir" class="btn btn-danger" href="#" role="button">Excluir</a>
+                </td>
+            <tr class="">
+                <td scope="row">Item</td>
+                <td>Item</td>
+                <td>Item</td>
+            </tr> -->
+        </tbody>
+    </table>
+</div>
 
-                // Selecionar n sei sei nao
-                $sql = "SELECT * FROM pedidos";
-                $stmt = $pdo->prepare($sql);
-                $stmt->execute();
 
-                //Executar//
-                while ($row = $stmt->fetch()) {
-                    echo "<tr>";
-                    echo "<td>" . $row["data"] . "</td>";
-                    echo "<td>" . $row["cliente"] . "</td>";
-                    echo "<td>" . $row["produto"] . "</td>";
-                    echo "<td>" . $row["valor"] . "</td>";
-                    echo "<td>";
-                    echo "<a name='editar' id='editar' class='btn btn-warning' href='editar.php' role='button'>".$row [id] ."Editar" ."</a>";
-                    echo "<a name='excluir' id='excluir' class='btn btn-danger' href='excluir.php' role='button'>".$row [id] ."Excluir</a>";
-                    echo "</td>";
-                    echo "</tr>";
-                }
-                ?>
-                <tr class="">
-                    <td scope="row">23/11/2023</td>
-                    <td>José</td>
-                    <td>Mesa</td>
-                    <td>R$ 23,00</td>
-                    <td>
-                        <a name="editar" id="editar" class="btn btn-warning" href="#" role="button">Editar</a>
-                        <a name="excluir" id="excluir" class="btn btn-danger" href="#" role="button">Excluir</a>
-                    </td>
-                </tr>
-                <tr class="">
-                    <td scope="row">Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+
+
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-
 </html>
