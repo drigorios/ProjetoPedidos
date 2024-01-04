bd324b899a953884903abf00adfa2fd076f9bcaf
rodrigo <alunodesign05@gmail.com>
2024-01-04 20:47:48 -0300

    ARQUIVO PARA EDITAR OS PEDIDOS (incompleto)

--- editar.php
+++ editar.php
@@ -1,13 +1,78 @@
+<?php
+//Connect
+
+$host = 'localhost';
+$dbname = 'pedidos';
+$username = 'root';
+$password = '';
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+} catch (PDOException $e) {
+    die("Erro ao conectar ao banco de dados" . $e->getMessage());
+}
+
+if (!isset($_GET['id'])) {
+    header('Location:index.php');
+    exit();
+}
+//GET ID
+$id = $_GET['id'];
+//PEGAR PRODUTO DA TABELA
+$sql = "SELECT FROM pedidos WHERE id = :id";
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':id', $id);
+$stmt->execute();
+$pedido = $stmt->fetch();
+//VERIFY
+if (!$pedido) {
+    header('Location:index.php');
+    exit();
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //GET DATA
+    $data = $_POST['data'];
+    $cliente = $_POST['cliente'];
+    $produto = $_POST['produto'];
+    $valor = $_POST['valor'];
+    //UPDATE DATA
+    $sql = "UPDATE pedidos SET data = :data,cliente = :cliente, produto= :produto,valor = :valor WHERE 'id'=:$id";
+
+    $stmt->$pdo->prepare($sql);
+    $stmt->bindValue(':data', $data);
+    $stmt->bindValue(':cliente', $cliente);
+    $stmt->bindValue(':produto', $produto);
+    $stmt->bindValue(':valor', $valor);
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
+
+    header('Location:index.php');
+    exit();
+}
+?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Pedidos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>   
+<body>
+    <h1>Editar Pedido</h1>
+    <form action="editar.php?id=" <?php echo $id ?> method="post">
+        <label for="data">Data</label>
+        <input type="date" name="data" value="<?php echo $pedido['data'] ?>" <br>
+        <label for="cliente">Cliente</label>
+        <input type="text" name="client" value="<?php echo $pedido['cliente'] ?>"
+         <br>
+        <label for="data">Produto</label>
+        <input type="text" name="produto" value="<?php echo $pedido['produto'] ?>" <br>
+        <label for="valor">valor</label>
+        <input type="text" name="valor" value="<?php echo $pedido['valor'] ?>"
+        <br>
+        <input type="submit" value="Salvar Alterações">
+    </form>
 </body>
+
 </html>
